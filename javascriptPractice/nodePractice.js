0a64b1af12b25bfd4f672d1c59dd3f360052f0e6
garengh4 <garengh4@gmail.com>
2023-04-18 11:35:53 -0400

    console

--- javascriptPractice/nodePractice.js
+++ javascriptPractice/nodePractice.js
@@ -1,4 +1,10 @@
-let firstName ="John"
+
+
 function addLastName(lastName, firstName){
     return firstName + " " + lastName;
 }
+
+
+
+// run `node nodePractice.js` in terminal
+console.log(addLastName("Smith","John"))
