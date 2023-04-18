5e2d778a4754186dcf0161ba12c34906b351c98e
garengh4 <garengh4@gmail.com>
2023-04-18 14:30:35 -0400

    nonsence stuff

--- javascriptPractice/nodePractice.js
+++ javascriptPractice/nodePractice.js
@@ -1,17 +1,21 @@
 
 // The new way, written for OS instead of Browser
-function addLastName(lastName, firstName){
+function addLastName(lastName, firstName) {
     return firstName + " " + lastName;
 }
 // run `node nodePractice.js` in terminal
-console.log(addLastName("Smith","John"))
+console.log(addLastName("Smith", "John"))
 
-// ---------------------------------------------------------------------------------------------------------
-// spread operator
-let nameArr = ["Alice","Bob","John"]
+// ----------------------------- spread operator----------------------------------------------------------------
+let nameArr = ["Alice", "Bob", "John"]
 console.log(nameArr)
 let arrCopy = [...nameArr]
 console.log(arrCopy)
 let arrCopy2 = ["mango", ...nameArr, "cheese"]
 console.log(arrCopy2)
 // ---------------------------------------------------------------------------------------------------------
+
+const fruits = ["Apple", "Mango", "Cherry"];
+fruits.forEach((item, index, array) => {
+    console.log(item, index);
+});
