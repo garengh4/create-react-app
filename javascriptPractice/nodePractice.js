2e0b7a5dde6b6b3cf2308a86a3ca92ddae7c610e
garengh4 <garengh4@gmail.com>
2023-04-18 13:09:00 -0400

    spread

--- javascriptPractice/nodePractice.js
+++ javascriptPractice/nodePractice.js
@@ -3,8 +3,15 @@
 function addLastName(lastName, firstName){
     return firstName + " " + lastName;
 }
-
-
-
 // run `node nodePractice.js` in terminal
 console.log(addLastName("Smith","John"))
+
+// ---------------------------------------------------------------------------------------------------------
+// spread operator
+let nameArr = ["Alice","Bob","John"]
+console.log(nameArr)
+let arrCopy = [...nameArr]
+console.log(arrCopy)
+let arrCopy2 = ["mango", ...nameArr, "cheese"]
+console.log(arrCopy2)
+// ---------------------------------------------------------------------------------------------------------
