d523d567a20a108cb41038ea3f9f28db5710719c
garengh4 <garengh4@gmail.com>
2023-04-20 12:51:41 -0400

    defects

---
+++ react-app/src/components/viewDefects.js
@@ -0,0 +1,9 @@
+export function viewDefects() {
+
+    let defects = {
+        "defects": [
+            { "id": 1, "category": "UI", "priority": 2, "status": "open", "description": "Submit button coming to extreme left; see screenshots" },
+            { "id": 2, "category": "Functional", "priority": 1, "status": "open", "description": "Some stuff" },
+            { "id": 3, "category": "Change Request", "priority": 3, "status": "closed", "description": "Add remove user functionality" }]
+    }
+}
